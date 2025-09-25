927fc8867655577c490c4d11ff302227ee9b8ca7
MaartenS11 <maarten.steevens@gmail.com>
2025-09-25 17:52:55 +0200

    WIP recent file list on the welcome screen

--- src/main/kotlin/be/ugent/topl/mio/ui/StartScreen.kt
+++ src/main/kotlin/be/ugent/topl/mio/ui/StartScreen.kt
@@ -3,16 +3,23 @@ package be.ugent.topl.mio.ui
 import be.ugent.topl.mio.DebuggerConfig
 import be.ugent.topl.mio.connections.ProcessConnection
 import be.ugent.topl.mio.connections.SerialConnection
+import be.ugent.topl.mio.sourcemap.AsSourceMapping
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.fazecast.jSerialComm.SerialPort
 import com.formdev.flatlaf.extras.FlatSVGIcon
-import be.ugent.topl.mio.sourcemap.AsSourceMapping
+import jdk.jfr.consumer.EventStream.openFile
 import java.awt.Dimension
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import java.io.File
 import java.io.FileInputStream
 import java.io.FileWriter
-import java.util.Properties
+import java.util.*
 import javax.swing.*
 import javax.swing.filechooser.FileNameExtensionFilter
+
 
 open class StartScreen(config: DebuggerConfig) : AboutScreen(config) {
     init {
@@ -48,11 +55,38 @@ open class StartScreen(config: DebuggerConfig) : AboutScreen(config) {
             isSelected = config.useEmulator || config.port == null
         }
         mainPanel.add(emulatorCheckbox)
+
+        // Add history:
+        val recentFile = File(DebuggerConfig.configDir + "/recent.json")
+        val mapper = ObjectMapper()
+        mapper.registerKotlinModule()
+        val recent = if (recentFile.exists()) {
+            mapper.readValue<Recent>(recentFile)
+        } else {
+            Recent(mutableListOf())
+        }
+
         val recentProperties = Properties()
         val recentConfig = DebuggerConfig.configDir + "/recent.properties"
         if (File(recentConfig).exists()) {
             recentProperties.load(FileInputStream(recentConfig))
         }
+
+        fun open(selectedFile: File) {
+            recentProperties.setProperty("lastDir", selectedFile.parent)
+            recentProperties.store(FileWriter(recentConfig), null)
+            if(!startDebugger(selectedFile, emulatorCheckbox.isSelected, portComboBox.selectedItem as String?)) {
+                JOptionPane.showMessageDialog(this, "Please select a port!", "Invalid port", JOptionPane.ERROR_MESSAGE)
+                return
+            }
+            if (recent.recentFiles.last() != selectedFile.absolutePath) {
+                recent.recentFiles.add(selectedFile.absolutePath)
+                mapper.writeValue(recentFile, recent)
+            }
+            isVisible = false
+            dispose()
+        }
+
         mainPanel.add(JButton("Select program").apply {
             setAlignmentX(CENTER_ALIGNMENT)
             addActionListener {
@@ -60,17 +94,27 @@ open class StartScreen(config: DebuggerConfig) : AboutScreen(config) {
                 chooser.fileSelectionMode = JFileChooser.FILES_ONLY
                 chooser.fileFilter = FileNameExtensionFilter("WebAssembly binaries (.wasm)", "wasm")
                 if (chooser.showOpenDialog(this) == JFileChooser.APPROVE_OPTION) {
-                    recentProperties.setProperty("lastDir", chooser.selectedFile.parent)
-                    recentProperties.store(FileWriter(recentConfig), null)
-                    if(!startDebugger(chooser.selectedFile, emulatorCheckbox.isSelected, portComboBox.selectedItem as String?)) {
-                        JOptionPane.showMessageDialog(this, "Please select a port!", "Invalid port", JOptionPane.ERROR_MESSAGE)
-                        return@addActionListener
-                    }
-                    isVisible = false
-                    dispose()
+                    open(chooser.selectedFile)
                 }
             }
         })
+
+        mainPanel.add(JLabel("Recent:"))
+        val model = DefaultListModel<String>()
+        for (file in recent.recentFiles) {
+            model.addElement(file)
+        }
+        mainPanel.add(JScrollPane(JList(model).apply {
+            addMouseListener(object : MouseAdapter() {
+                override fun mouseClicked(evt: MouseEvent) {
+                    val list = evt.getSource() as JList<*>
+                    if (evt.getClickCount() == 2) {
+                        val index = list.locationToIndex(evt.getPoint())
+                        open(File(recent.recentFiles[index]))
+                    }
+                }
+            })
+        }))
     }
 
     private fun startDebugger(binary: File, emulator: Boolean, comPort: String?): Boolean {
@@ -88,3 +132,5 @@ open class StartScreen(config: DebuggerConfig) : AboutScreen(config) {
         return true
     }
 }
+
+data class Recent(val recentFiles: MutableList<String>)
