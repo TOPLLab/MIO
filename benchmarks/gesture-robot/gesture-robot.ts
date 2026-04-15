465153a0394d06c107d29e8b261fac167844d608
MaartenS11 <maarten.steevens@gmail.com>
2026-04-15 11:37:44 +0200

    Update gesture-robot evaluation

--- benchmarks/gesture-robot/gesture-robot.ts
+++ benchmarks/gesture-robot/gesture-robot.ts
@@ -2,9 +2,14 @@
 @external("env", "chip_pin_mode") declare function chip_pin_mode(pin: i32, mode: i32): void;
 @external("env", "chip_digital_write") declare function chip_digital_write(pin: i32, value: i32): void;
 @external("env", "chip_analog_read") declare function chip_analog_read(pin: i32): i32;
+@external("env", "print_string") export declare function print_string(text: ArrayBuffer, length: u32): void;
+
+function print(text: string): void {
+    print_string(String.UTF8.encode(text, true), String.UTF8.byteLength(text, true));
+}
 
 const INPUT = 0;
-const OUTPUT = 2;
+const OUTPUT = 1;
 
 enum Pin {
     ap1 = 0,
@@ -20,19 +25,27 @@ let ov1: i32 = 0;
 let sv2: i32 = 0;
 let ov2: i32 = 0;
 
+function map(value: i32, fromLow: i32, fromHigh: i32, toLow: i32, toHigh: i32): i32 {
+    return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
+}
+
 export function main(): void {
     chip_pin_mode(Pin.pin13, OUTPUT);
     chip_pin_mode(Pin.pin12, OUTPUT);
     chip_pin_mode(Pin.pin11, OUTPUT);
     chip_pin_mode(Pin.pin10, OUTPUT);
 
+    //while (true) {
     while (true) {
         sv1 = chip_analog_read(Pin.ap1);
-        ov1 = (sv1 * 255) / 1023;
+        ov1 = map(sv1, 0, 1023, 0, 255);
         chip_delay(2);
         sv2 = chip_analog_read(Pin.ap2);
-        ov2 = (sv2 * 255) / 1023;
+        ov2 = map(sv2, 0, 1023, 0, 255);
         chip_delay(2);
+
+        //print("Xsensor1 = " + sv1.toString() + "\t output1 = " + ov1.toString() + "\n");
+        //print("Ysensor2 = " + sv2.toString() + "\t output2 = " + ov2.toString() + "\n");
 
         if (chip_analog_read(Pin.ap1) < 514 && chip_analog_read(Pin.ap2) < 463) {
             // for backward movement
@@ -64,5 +77,7 @@ export function main(): void {
             chip_digital_write(Pin.pin11, 1);
             chip_digital_write(Pin.pin10, 1);
         }
+        // Stop after the first loop iteration, limiting the variable count is not enough for this program.
+        return
     }
 }
