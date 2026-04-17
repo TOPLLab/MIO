c935b832c988f1180ad9fbecda70d84b5a93396e
MaartenS11 <maarten.steevens@gmail.com>
2026-04-17 17:32:28 +0200

    Print table header and generate tex file

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -22,6 +22,21 @@ programs = [
 wdcli = "../WARDuino-symbolic/build-emu/wdcli"
 
 def analyse_state_space():
+    f = open("table.tex", "w")
+    print("\\documentclass[12pt]{article}", file=f)
+    print("\\usepackage{siunitx}", file=f)
+    print("\\sisetup{", file=f)
+    print("scientific-notation = true,", file=f)
+    print("round-mode = figures,", file=f)
+    print("round-precision = 4,", file=f)
+    print("retain-zero-exponent = true", file=f)
+    print("}", file=f)
+    print("\\begin{document}", file=f)
+    print("\\begin{tabular}{l S[table-format=1.1] r r r}", file=f)
+    print("Program & {States} & Paths & Max opts & Time (s) \\\\", file = f)
+    print("\\hline", file = f)
+
+    print("Program & States & Paths & Max opts & Time (s) \\\\")
     for program in programs:
         print(f"Running {program[0]}", end="", flush=True)
         command = wdcli + " " + program[0] + f" --no-socket --no-debug --mode concolic --max-instructions {program[1]} --max-symbolic-variables {program[2]} --max-iterations {program[3]}"
@@ -31,5 +46,9 @@ def analyse_state_space():
         process = subprocess.run([command], shell=True, check=True, capture_output=True, text=True)
         output_lines = process.stdout.strip().split('\n')
         print(f"\r{program[0].replace("/upload.wasm", "")} & {output_lines[-1]}")
+        print(f"{program[0].replace("/upload.wasm", "")} & {output_lines[-1]}", file = f)
+    print("\\end{tabular}", file=f)
+    print("\\end{document}", file=f)
+    f.close()
 
 analyse_state_space()
