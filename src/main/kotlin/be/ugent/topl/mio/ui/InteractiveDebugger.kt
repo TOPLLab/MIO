9a44b7a24f9676ca755c64c8d411845555626051
MaartenS11 <maarten.steevens@gmail.com>
2026-07-01 10:53:43 +0200

    Catch java.lang.Error which can be thrown by the UI

--- src/main/kotlin/be/ugent/topl/mio/ui/InteractiveDebugger.kt
+++ src/main/kotlin/be/ugent/topl/mio/ui/InteractiveDebugger.kt
@@ -424,6 +424,7 @@ class InteractiveDebugger(
                     scrollPane.verticalScrollBar.value = textArea.yForLine((lineNumber - 11).coerceAtLeast(0))
                 }
                 catch (_: Exception) {}
+                catch (_: java.lang.Error) {}
             }
         } catch(iae : RuntimeException) {
             /*
