a5adb6c57a1fc9eb05dbc666e1e87ea832d8be36
MaartenS11 <maarten.steevens@gmail.com>
2026-08-07 13:44:48 +0200

    Fix primitive overrides being incorrectly encoded when having more than one argument

--- src/main/kotlin/be/ugent/topl/mio/debugger/Debugger.kt
+++ src/main/kotlin/be/ugent/topl/mio/debugger/Debugger.kt
@@ -466,7 +466,7 @@ open class Debugger(private val connection: Connection, start: Boolean = true, p
 
     open fun addPrimitiveOverride(primName: String, args: List<Int>, returnValue: Int): Boolean {
         val primNameSerialised = primName.chars().toList().joinToString("") { c: Int -> String.format("%02x", c) } + "00"
-        val payload = primNameSerialised + args.joinToString { String.format("%08x", it) } + String.format("%08x", returnValue)
+        val payload = primNameSerialised + args.joinToString("") { String.format("%08x", it) } + String.format("%08x", returnValue)
         send(80, payload)
         return messageQueue.waitForAck("80")[0] == "1"
     }
